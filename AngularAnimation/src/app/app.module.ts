9e21fe8b55a850cda831d1d7e57e00d1a73a2827
Lina Sachuk <sachuklina@gmail.com>
2021-03-02 19:14:09 -0700

    add navbar

--- AngularAnimation/src/app/app.module.ts
+++ AngularAnimation/src/app/app.module.ts
@@ -12,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
